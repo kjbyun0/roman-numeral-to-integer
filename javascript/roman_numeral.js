0d2a5ccbc7203f9c74a3428a32ac04f0e345c50f
KI JONG BYUN <kjbyun0@gmail.com>
2024-08-27 14:31:20 -0500

    Add my solution

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,24 @@
 function romanNumeral(string) {
   // type your code here
+  const symbols = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  }
+
+  let res = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (i + 1 < string.length && symbols[string[i]] < symbols[string[i+1]])
+      res -= symbols[string[i]];
+    else
+      res += symbols[string[i]];
+  }
+
+  return res;
 }
 
 if (require.main === module) {
@@ -16,6 +35,18 @@ if (require.main === module) {
 
   console.log("Expecting: 402");
   console.log(romanNumeral('CDII'));
+
+  console.log("");
+  console.log("Expecting: 4");
+  console.log(romanNumeral('IV'));
+
+  console.log("");
+  console.log("Expecting: 9");
+  console.log(romanNumeral('IX'));
+
+  console.log("");
+  console.log("Expecting: 6");
+  console.log(romanNumeral('VI'));
 }
 
 module.exports = romanNumeral;
